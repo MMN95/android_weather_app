f4c5a4e4301224bc2bd59cdc1fcfe00c27a073b2
Michael Makulov <mmn95@mail.ru>
2021-07-28 21:55:39 +0500

    -Проект переделан под архитектуру MVVM

--- app/src/main/java/ru/mmn/weatherapp/MainActivity.kt
+++ app/src/main/java/ru/mmn/weatherapp/MainActivity.kt
@@ -2,23 +2,16 @@ package ru.mmn.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.RecoverySystem
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import ru.mmn.weatherapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val button:Button = findViewById(R.id.button)
-        val textView:TextView = findViewById(R.id.text_view)
-
-        button.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                textView.text = Repository.getWeatherList().toString()
-            }
-        })
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
     }
 }

d9c3eef712caf280b494289828766074e0c8b1b9
Michael Makulov <mmn95@mail.ru>
2021-07-28 22:35:29 +0500

    -Добавление связи между ViewModel и LiveData

--- app/src/main/java/ru/mmn/weatherapp/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/mmn/weatherapp/ui/main/MainViewModel.kt
@@ -1,7 +1,21 @@
 package ru.mmn.weatherapp.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel (private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData()) : ViewModel() {
+
+    fun getData(): LiveData<Any> {
+        getDataFromLocalSource()
+        return liveDataToObserve
+    }
+
+    private fun getDataFromLocalSource() {
+        Thread {
+            sleep(1000)
+            liveDataToObserve.postValue(Any())
+        }.start()
+    }
 }
